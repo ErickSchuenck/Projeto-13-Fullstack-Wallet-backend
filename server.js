7e39c5ee710a6e69dc50da88b8d65f88195907a7
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-09 16:45:12 -0300

    port bugfix

--- server.js
+++ server.js
@@ -7,33 +7,8 @@ import userRoutes from './routers/userRouters.js'
 
 dotenv.config();
 const app = express()
-// funções
 app.use(cors());
 app.use(express.json());
-// criar função para: Enviar email e senha. Cadastrar usuário com nome, email e senha, enviar entrada, enviar saida, pegar registros
-app.listen(5000, () => console.log('server ligado na porta 5000'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('server ligado'));
 app.use(userRoutes)
-
-// app.post('/transactions', async (req, res) => {
-//   const { name, token, type, value, description
-//   } = req.body
-//   await users.insertOne({
-//     type: type,
-//     name: name,
-//     token: token,
-//     value: value,
-//     description: description,
-//   })
-// })
-
-// app.get('/transactions', async (req, res) => {
-//   try {
-//     const lastTransactions = await transactions
-//       .find()
-//       .toArray()
-//     res.send(lastTransactions)
-//   }
-//   catch {
-//     res.sendStatus(500)
-//   }
-// })
