ce8da42e0c3fb69c143e7e8d3ed6913ab3c43f3e
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-09 13:28:57 -0300

    bugfix

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -2,10 +2,11 @@ import express from 'express'
 import { login } from '../controllers/userLoginController.js'
 import { registerUser } from '../controllers/userRegisterController.js'
 import { deposit, withdraw, getFinances } from './../controllers/userFinancesController.js'
+import { registerValidation } from "../middlewares/registerValidation.js";
 
 const userRoutes = express.Router()
 
-userRoutes.post('/users', registerUser)
+userRoutes.post('/users', registerValidation, registerUser)
 userRoutes.post('/login', login)
 userRoutes.post('/deposit', deposit)
 userRoutes.post('/withdraw', withdraw)
