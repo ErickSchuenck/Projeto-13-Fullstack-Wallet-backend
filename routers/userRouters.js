c6d72bdabc4b4aff8ae5f17f678c108afb72b1af
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-09 16:03:48 -0300

    login validation

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -3,11 +3,12 @@ import { login } from '../controllers/userLoginController.js'
 import { registerUser } from '../controllers/userRegisterController.js'
 import { deposit, withdraw, getFinances } from './../controllers/userFinancesController.js'
 import { registerValidation } from "../middlewares/registerValidation.js";
+import { loginValidation } from '../middlewares/loginValidation.js';
 
 const userRoutes = express.Router()
 
 userRoutes.post('/users', registerValidation, registerUser)
-userRoutes.post('/login', login)
+userRoutes.post('/login', loginValidation, login)
 userRoutes.post('/deposit', deposit)
 userRoutes.post('/withdraw', withdraw)
 userRoutes.get('/getFinances', getFinances)
