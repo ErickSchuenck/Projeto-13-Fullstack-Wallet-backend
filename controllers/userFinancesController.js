c237b89ae01fa05f080b67c3bfcd90880325adbf
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-08 23:35:24 -0300

    agora adiciona novas transações

--- controllers/userFinancesController.js
+++ controllers/userFinancesController.js
@@ -4,14 +4,12 @@ const users = db.collection("users");
 
 export async function withdraw(req, res) {
   console.log(req.body)
-
-  const { value, type, title } = req.body
+  const { value, type, title, token } = req.body
 
 }
 
 export async function deposit(req, res) {
   console.log(req.body)
-
-  const { value, type, title } = req.body
-
+  const { value, type, title, token } = req.body
+  users.updateOne({ token }, { $push: { finances: [{ value, type, title }] } })
 }
